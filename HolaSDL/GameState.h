40020915349523875123304fce7140cf7a3ea606
Angelo06 <angelo06@ucm.es>
2021-01-06 14:20:17 +0100

    Atributos del Game State pero no se los ha follado el diablo

--- HolaSDL/GameState.h
+++ HolaSDL/GameState.h
@@ -1,6 +1,17 @@
 #pragma once
+#include <list>;
+#include "EventHandler.h";
+#include "GameObject.h";
+using namespace std;
+
 class GameState
 {
+	list<GameObject*> gO;
+	list<EventHandler*> events;
 
+protected:
+	virtual void Render() = 0;
+	virtual void Update() = 0;
+	virtual void HandleEvents() = 0;
 };
 
